69474742812cedfb082f3d04538f25790865447c
eyesniper2 <eyesniper2@rayfall.net>
2018-02-21 14:01:32 -0700

    Added in Robolectric

--- app/src/test/java/ca/ualberta/taskr/ExampleUnitTest.kt
+++ app/src/test/java/ca/ualberta/taskr/ExampleUnitTest.kt
@@ -3,15 +3,16 @@ package ca.ualberta.taskr
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+
+@RunWith(RobolectricTestRunner::class)
 class ExampleUnitTest {
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
 }
