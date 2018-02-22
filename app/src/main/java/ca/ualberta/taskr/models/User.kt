d71a7f6425b93e1f0ceabb4b61a7d086d05ceaaa
Brendan Samek <xrendan@gmail.com>
2018-02-21 17:11:43 -0700

    Fix capitalization issues

--- app/src/main/java/ca/ualberta/taskr/models/User.kt
+++ app/src/main/java/ca/ualberta/taskr/models/User.kt
@@ -4,4 +4,4 @@ import android.media.Image
 
 
 data class User(val name: String, val phoneNumber: String, val profilePicture: Image,
-                val Email: String, val username: String)
+                val email: String, val username: String)
