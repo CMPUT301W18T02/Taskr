0a44728ed86246dd8941e26fa76f85a0aac0fb71
Marissa Snihur <marissasnihur@Marissas-MacBook-Pro.local>
2018-02-22 12:55:29 -0700

    Fix class error

--- app/src/main/java/ca/ualberta/taskr/models/User.kt
+++ app/src/main/java/ca/ualberta/taskr/models/User.kt
@@ -3,5 +3,5 @@ package ca.ualberta.taskr.models
 import android.media.Image
 
 
-data class User(var name: String, var phoneNumber: String, var profilePicture: Image?,
-                var email: String, val username: String)
+data class User(val name: String, val phoneNumber: String, val profilePicture: Image?,
+                val email: String, val username: String)
