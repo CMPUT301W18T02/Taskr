02548c94a9f802c4e5924a060a67220285ac592c
MushMaw <jtbakker@ualberta.ca>
2018-02-22 17:45:08 -0700

    Added tests for Tasks, getter methods for arrays

--- app/src/main/java/ca/ualberta/taskr/models/Task.kt
+++ app/src/main/java/ca/ualberta/taskr/models/Task.kt
@@ -4,6 +4,31 @@ import android.media.Image
 import com.mapbox.mapboxsdk.geometry.LatLng
 
 
-data class Task(val owner: String, val title: String, var status: TaskStatus,
+data class Task(val owner: String, val title: String, var status: TaskStatus?,
                 val bids: ArrayList<Bid>, val description: String, val photos: ArrayList<Image>,
-                val location: LatLng, val chosenBidder: String )
+                val location: LatLng?, val chosenBidder: String ) {
+
+    fun getBidAtIndex(index: Int): Bid {
+        return this.bids[index]
+    }
+
+    fun addBid(newBid: Bid) {
+        this.bids.add(newBid)
+    }
+
+    fun setBidAtIndex(newBid: Bid, index: Int) {
+        this.bids[index] = newBid;
+    }
+
+    fun getPhotoAtIndex(index: Int): Image {
+        return this.photos[index]
+    }
+
+    fun setPhotoAtIndex(newPhoto: Image, index: Int) {
+        this.photos[index] = newPhoto
+    }
+
+    fun addPhoto(newPhoto: Image) {
+        this.photos.add(newPhoto)
+    }
+}
