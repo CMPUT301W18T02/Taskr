ae9d0f48bdc7e4b8508c510cd4590bf1d15dd8b8
MichaelSteer <swedester44@gmail.com>
2018-03-08 02:12:44 -0700

    weird bug involving GG.

--- app/src/main/java/ca/ualberta/taskr/LoginActivity.kt
+++ app/src/main/java/ca/ualberta/taskr/LoginActivity.kt
@@ -3,8 +3,13 @@ package ca.ualberta.taskr
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import org.androidannotations.annotations.Click
+import org.androidannotations.annotations.ViewById
 
 class LoginActivity : AppCompatActivity() {
+
+    @ViewById val LoginButton = null;
+
+    @ViewById val NewUserButton = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
