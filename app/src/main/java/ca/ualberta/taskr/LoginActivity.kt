c229f89b5ea1262c729f15b3d7bc42b9d3ec9e5d
MichaelSteer <swedester44@gmail.com>
2018-03-07 23:31:41 -0700

    Fixed a bad name

--- app/src/main/java/ca/ualberta/taskr/LoginActivity.kt
+++ app/src/main/java/ca/ualberta/taskr/LoginActivity.kt
@@ -7,6 +7,6 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
