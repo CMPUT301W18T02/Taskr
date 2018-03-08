91190f88875757b52679bfa056ef87167c599397
MichaelSteer <swedester44@gmail.com>
2018-03-08 00:22:43 -0700

    Added barebones for LoginActivity

--- app/src/main/java/ca/ualberta/taskr/LoginActivity.kt
+++ app/src/main/java/ca/ualberta/taskr/LoginActivity.kt
@@ -1,12 +1,23 @@
 package ca.ualberta.taskr
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.androidannotations.annotations.Click
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    @Click(R.id.LoginButton)
+    fun LoginClicked() {
+
+    }
+
+    @Click(R.id.NewUserButton)
+    fun NewUserClicked() {
+
     }
 }
