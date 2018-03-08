bc0e18df862e350acaa36d4b4b8b0cd9fe1fb921
MichaelSteer <swedester44@gmail.com>
2018-03-08 01:38:40 -0700

    Added barebones for EditUserActivity and MyTasksActivity

--- app/src/main/java/ca/ualberta/taskr/EditUserActivity.kt
+++ app/src/main/java/ca/ualberta/taskr/EditUserActivity.kt
@@ -1,12 +1,23 @@
 package ca.ualberta.taskr
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.androidannotations.annotations.Click
 
 class EditUserActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_user)
+    }
+
+    @Click(R.id.ApplyChangesButton)
+    fun onApplyChangesClicked() {
+
+    }
+
+    @Click(R.id.ProfileImageButton)
+    fun onPhotoClicked() {
+
     }
 }
