8446726595b0ce7ec153932d0ed4f2bddeaf67f5
MichaelSteer <swedester44@gmail.com>
2018-03-10 03:06:51 -0700

    Semi finalized theme

--- app/src/main/java/ca/ualberta/taskr/LoginActivity.kt
+++ app/src/main/java/ca/ualberta/taskr/LoginActivity.kt
@@ -2,27 +2,53 @@ package ca.ualberta.taskr
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import org.androidannotations.annotations.Click
 import org.androidannotations.annotations.ViewById
 
 class LoginActivity : AppCompatActivity() {
 
-    @ViewById val LoginButton = null;
-
-    @ViewById val NewUserButton = null;
+    @ViewById lateinit var LoginButton    : Button
+    @ViewById lateinit var NewUserButton  : Button
+    @ViewById lateinit var LoginErrorText : TextView
+    @ViewById lateinit var UsernameText   : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
 
+    fun CheckIfUsernameExists(username: String) {
+
+    }
+
+    fun getConnectivityStatus() {
+
+    }
+
+    fun showLoginError(message: String) {
+
+    }
+
     @Click(R.id.LoginButton)
-    fun LoginClicked() {
+    fun LoginClicked(v: View) {
         android.util.Log.d("CLICK", "Login button clicked")
     }
 
     @Click(R.id.NewUserButton)
-    fun NewUserClicked() {
+    fun NewUserClicked(v: View) {
         android.util.Log.d("CLICK", "NewUser button clicked")
+    }
+
+    @Click(R.id.ImageConnectionStatus)
+    fun onTaskrImageClick() {
+
+    }
+
+    fun bundleActivity() {
+
     }
 }
