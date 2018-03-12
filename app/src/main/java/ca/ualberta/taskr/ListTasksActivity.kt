d7594ad39fb7c9cf7c4966e3edb992026142d4bb
eyesniper2 <eyesniper2@rayfall.net>
2018-03-11 19:56:20 -0600

    Built up task list recycler view and list items

--- app/src/main/java/ca/ualberta/taskr/ListTasksActivity.kt
+++ app/src/main/java/ca/ualberta/taskr/ListTasksActivity.kt
@@ -2,11 +2,33 @@ package ca.ualberta.taskr
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import ca.ualberta.taskr.adapters.TaskListAdapter
+import ca.ualberta.taskr.models.Task
+import org.androidannotations.annotations.ViewById
 
 class ListTasksActivity : AppCompatActivity() {
+
+    @ViewById
+    private lateinit var taskList: RecyclerView
+
+    private var masterTaskList: ArrayList<Task> = ArrayList()
+    private var taskListAdapter: TaskListAdapter = TaskListAdapter(masterTaskList)
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_tasks)
+
+        // Build up recycle view
+        viewManager = LinearLayoutManager(this)
+        taskList.apply {
+            layoutManager = viewManager
+            adapter = taskListAdapter
+        }
+
+
     }
 }
