347bc984d5703a51bf958142c4b33140aaf65e09
eyesniper2 <eyesniper2@rayfall.net>
2018-03-14 12:35:58 -0600

    First pass on base nav drawer

--- app/src/main/java/ca/ualberta/taskr/ListTasksActivity.kt
+++ app/src/main/java/ca/ualberta/taskr/ListTasksActivity.kt
@@ -1,13 +1,17 @@
 package ca.ualberta.taskr
 
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.util.Log
+import android.view.MenuItem
 import ca.ualberta.taskr.adapters.TaskListAdapter
 import ca.ualberta.taskr.models.Task
 import ca.ualberta.taskr.models.elasticsearch.GenerateRetrofit
-import org.androidannotations.annotations.ViewById
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,8 +19,8 @@ import retrofit2.Response
 
 class ListTasksActivity : AppCompatActivity() {
 
-    @ViewById
     private lateinit var taskList: RecyclerView
+    private lateinit var mDrawerLayout: DrawerLayout
 
     private var masterTaskList: ArrayList<Task> = ArrayList()
     private var taskListAdapter: TaskListAdapter = TaskListAdapter(masterTaskList)
@@ -27,6 +31,15 @@ class ListTasksActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_tasks)
 
+        taskList = findViewById(R.id.taskList)
+        mDrawerLayout = findViewById(R.id.drawer_layout)
+
+        var toolbar = findViewById<Toolbar>(R.id.taskListToolbar)
+        setSupportActionBar(toolbar)
+        var actionbar = supportActionBar
+        actionbar!!.setDisplayHomeAsUpEnabled(true)
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu)
+
         // Build up recycle view
         viewManager = LinearLayoutManager(this)
         taskList.apply {
@@ -36,7 +49,9 @@ class ListTasksActivity : AppCompatActivity() {
 
         GenerateRetrofit.generateRetrofit().getTasks().enqueue(object : Callback<List<Task>> {
             override fun onResponse(call: Call<List<Task>>, response: Response<List<Task>>) {
-                masterTaskList = response.body() as ArrayList<Task>
+                Log.i("network", response.body().toString())
+                masterTaskList.clear()
+                masterTaskList.addAll(response.body() as ArrayList<Task>)
                 taskListAdapter.notifyDataSetChanged()
             }
 
@@ -46,5 +61,15 @@ class ListTasksActivity : AppCompatActivity() {
         })
 
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
