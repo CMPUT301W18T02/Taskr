df763dbad245db5ea0e84f7ab1c6cce06478698d
CptJamesCook <mrniceguyjames@gmail.com>
2018-03-15 18:08:12 -0600

    Added functions

--- app/src/main/java/ca/ualberta/taskr/MyTasksActivity.kt
+++ app/src/main/java/ca/ualberta/taskr/MyTasksActivity.kt
@@ -2,11 +2,37 @@ package ca.ualberta.taskr
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ListView
+import org.androidannotations.annotations.Click
+import org.androidannotations.annotations.ViewById
 
 class MyTasksActivity : AppCompatActivity() {
+
+    @ViewById lateinit var AddTaskButton : Button
+    @ViewById lateinit var MyTasksListView : ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_tasks)
+
+        populateList()
+    }
+
+    /**
+     * Populate the task list with the user's current active tasks.
+     */
+    private fun populateList(){
+        //use a method that gets users tasks
+        //populate list with returned tasks
+    }
+
+    /**
+     * On clicking the Add Task Button, open a blank edit task activity.
+     */
+    @Click(R.id.AddTaskButton)
+    private fun openEditTaskActivity(v: View){
+        //open a blank edit task activity
     }
 }
