a3125e927262f25c3e0852e9a8b1e197b6d326ec
MushMaw <jtbakker@ualberta.ca>
2018-03-16 00:56:36 -0600

    Some more changes

---
+++ app/src/main/java/ca/ualberta/taskr/adapters/BidListAdapter.kt
@@ -0,0 +1,13 @@
+package ca.ualberta.taskr.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.widget.BaseAdapter
+import ca.ualberta.taskr.models.Bid
+
+/**
+ * Created by Jacob Bakker on 3/15/2018.
+ */
+class BidListAdapter(bidList: ArrayList<Bid>): RecyclerView.Adapter<BidListAdapter>.LocalViewHolder() {
+
+    private var bidList: List<Bid = >
+}
