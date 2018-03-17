a188869e5a6928087ea9b6d63ddb2a8863027a74
Marissa Snihur <msnihur@ualberta.ca>
2018-03-16 19:17:36 -0600

    First pass of GeoLocation

--- app/src/main/java/ca/ualberta/taskr/NearbyTasksActivity.kt
+++ app/src/main/java/ca/ualberta/taskr/NearbyTasksActivity.kt
@@ -1,77 +1,61 @@
 package ca.ualberta.taskr
 
-import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import ca.ualberta.taskr.Perms.PermsUtil
+
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.constants.Style
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
-import com.mapbox.mapboxsdk.style.layers.CircleLayer
-import com.mapbox.mapboxsdk.style.layers.PropertyFactory.*
-import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
-import com.mapbox.mapboxsdk.style.sources.VectorSource;
-import ca.ualberta.taskr.R.id.mapView
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 
 
 class NearbyTasksActivity : AppCompatActivity() {
 
-    private lateinit var mapView: MapView
-    private lateinit var map: MapboxMap
-    private lateinit var vectorSource: VectorSource
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        PermsUtil.getPermissions(this@NearbyTasksActivity)
-        Mapbox.getInstance(this, getString(R.string.access_token))
-        setContentView(R.layout.activity_nearby_tasks)
-        mapView = findViewById<MapView>(R.id.rangeMapView) as MapView
-        mapView.onCreate(savedInstanceState)
-        //mapView.setStyleUrl(Style.DARK)
-
-       /* mapView.getMapAsync(object : OnMapReadyCallback {
-            override fun onMapReady(mapboxMap: MapboxMap) {
-                mapboxMap.setStyleUrl(Style.DARK)
-            }
-        })*/
+    private var mapView: MapView? = null
 
 
-    }
-
-    override fun onStart(){
+    public override fun onStart() {
         super.onStart()
-        mapView.onStart()
+        mapView!!.onStart()
     }
 
-    override fun onResume(){
+    public override fun onResume() {
         super.onResume()
-        mapView.onResume()
+        mapView!!.onResume()
     }
 
-    override fun onPause(){
+    public override fun onPause() {
         super.onPause()
-        mapView.onPause()
+        mapView!!.onPause()
     }
 
-    override fun onStop(){
+    public override fun onStop() {
         super.onStop()
-        mapView.onStop()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView.onSaveInstanceState(outState)
+        mapView!!.onStop()
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
-        mapView.onLowMemory()
+        mapView!!.onLowMemory()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        mapView.onDestroy()
+        mapView!!.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        mapView!!.onSaveInstanceState(outState!!)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Mapbox.getInstance(this, "pk.eyJ1IjoiYmFybmFidXN0aGViZW5pZ24iLCJhIjoiY2pldWI2MHN2NGhrZDJxbWU4dHdubmwxYSJ9.ZVq95tHTxTgyyppAfj3Jdw")
+        setContentView(R.layout.activity_nearby_tasks)
+        mapView = findViewById<MapView>(R.id.mapView)
+        mapView!!.onCreate(savedInstanceState)
+        mapView!!.setStyleUrl("mapbox://styles/barnabusthebenign/cjeueb9xh0f872sl7fa70cgzb")
     }
 }
