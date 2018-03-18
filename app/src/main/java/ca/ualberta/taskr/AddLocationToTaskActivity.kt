0c4b4137da835942efb09298aa9ca909f7187e50
Marissa Snihur <msnihur@ualberta.ca>
2018-03-18 14:36:18 -0600

    Adding Location and sending LatLng back Properly

--- app/src/main/java/ca/ualberta/taskr/AddLocationToTaskActivity.kt
+++ app/src/main/java/ca/ualberta/taskr/AddLocationToTaskActivity.kt
@@ -3,100 +3,96 @@ package ca.ualberta.taskr
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+
+import butterknife.BindView
+import butterknife.ButterKnife
 import butterknife.OnClick
-import butterknife.OnItemClick
-import ca.ualberta.taskr.R.id.mapView
 import com.mapbox.mapboxsdk.Mapbox
 
-import com.mapbox.mapboxsdk.constants.Style
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
-import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 
 
-class AddLocationToTaskActivity : AppCompatActivity() {
-    private var mapView: MapView? = null
+class AddLocationToTaskActivity : AppCompatActivity(), OnMapReadyCallback, MapboxMap.OnMapClickListener {
+    @BindView(R.id.rangeMapView)
+    lateinit var mapView: MapView
     private var mapboxMap: MapboxMap? = null
-    private lateinit var button: Button
-    private lateinit var point: LatLng
+    @BindView(R.id.add_location)
+    private lateinit var position: LatLng
+
+
+    @OnClick(R.id.add_location)
+    fun sendBack(){
+        val intent = Intent(applicationContext, EditTaskActivity::class.java)
+        intent.putExtra("LatLng", position)
+        startActivity(intent)
+
+    }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Mapbox.getInstance(this, "pk.eyJ1IjoiYmFybmFidXN0aGViZW5pZ24iLCJhIjoiY2pldWI2MHN2NGhrZDJxbWU4dHdubmwxYSJ9.ZVq95tHTxTgyyppAfj3Jdw")
         setContentView(R.layout.activity_add_location_to_task)
-        mapView = findViewById<View>(R.id.rangeMapView) as MapView
-        button = findViewById(R.id.add_location)
-        println("HERE")
-        mapView!!.onCreate(savedInstanceState)
+        ButterKnife.bind(this)
+        mapView.onCreate(savedInstanceState)
 
-        //TODO implement onMapClickListener - then done.
-
-        /*@OnClick(R.id.rangeMapView)
-        fun mapClicked(view: View) {
-            println("CLICKED")
-            mapView!!.getMapAsync(OnMapReadyCallback { mapboxMap ->
-                mapboxMap.addMarker(MarkerOptions()
-                        .position(point)
-                        .title("Location")
-                        .snippet("YOU ARE HERE"))
-            })
-        }*/
-
-        mapView!!.getMapAsync(OnMapReadyCallback { mapboxMap ->
-            mapboxMap.addMarker(MarkerOptions()
-                    .position(LatLng(52.4631, -113.7286))
-                    .title("Location")
-                    .snippet("YOU ARE HERE"))
-        })
-
-        button.setOnClickListener(object: View.OnClickListener {
-            override fun onClick(view: View): Unit {
-                val intent = Intent(applicationContext, AddLocationToTaskActivity::class.java)
-                intent.putExtra("LatLng", point)
-                //TODO send intent back [with coords] .
-            }
-        })
+        mapView.getMapAsync(this)
 
 
     }
 
     public override fun onStart() {
         super.onStart()
-        mapView!!.onStart()
+        mapView.onStart()
     }
 
     public override fun onResume() {
         super.onResume()
-        mapView!!.onResume()
+        mapView.onResume()
     }
 
     public override fun onPause() {
         super.onPause()
-        mapView!!.onPause()
+        mapView.onPause()
     }
 
     public override fun onStop() {
         super.onStop()
-        mapView!!.onStop()
+        mapView.onStop()
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
-        mapView!!.onLowMemory()
+        mapView.onLowMemory()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        mapView!!.onDestroy()
+        mapView.onDestroy()
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
-        mapView!!.onSaveInstanceState(outState!!)
+        mapView.onSaveInstanceState(outState!!)
     }
+
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        this.mapboxMap = mapboxMap
+        mapboxMap.addOnMapClickListener(this)
+
+    }
+
+    override fun onMapClick(point: LatLng) {
+        mapboxMap!!.addMarker(MarkerOptions()
+                .position(point)
+                .title("Location")
+                .snippet("YOU ARE HERE"))
+        position = point
+    }
+
 }
