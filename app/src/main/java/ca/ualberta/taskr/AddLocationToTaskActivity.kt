31804728b91ea926efac6e4d2c58a9e467f8d93f
Marissa Snihur <msnihur@ualberta.ca>
2018-03-17 21:03:31 -0600

    Updated Map theme

--- app/src/main/java/ca/ualberta/taskr/AddLocationToTaskActivity.kt
+++ app/src/main/java/ca/ualberta/taskr/AddLocationToTaskActivity.kt
@@ -18,69 +18,72 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 
 
 class AddLocationToTaskActivity : AppCompatActivity() {
-    private lateinit var mapView: MapView
-    private lateinit var mapboxMap: MapboxMap
+    private var mapView: MapView? = null
+    private var mapboxMap: MapboxMap? = null
     private lateinit var button: Button
     private lateinit var point: LatLng
 
-
-    public override fun onStart() {
-        super.onStart()
-        mapView.onStart()
-    }
-
-    public override fun onResume() {
-        super.onResume()
-        mapView.onResume()
-    }
-
-    public override fun onPause() {
-        super.onPause()
-        mapView.onPause()
-    }
-
-    public override fun onStop() {
-        super.onStop()
-        mapView.onStop()
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView.onLowMemory()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView.onDestroy()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle?) {
-        super.onSaveInstanceState(outState)
-        mapView.onSaveInstanceState(outState!!)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Mapbox.getInstance(this, getString(R.string.access_token))
+        Mapbox.getInstance(this, "pk.eyJ1IjoiYmFybmFidXN0aGViZW5pZ24iLCJhIjoiY2pldWI2MHN2NGhrZDJxbWU4dHdubmwxYSJ9.ZVq95tHTxTgyyppAfj3Jdw")
         setContentView(R.layout.activity_add_location_to_task)
-        mapView = findViewById<MapView>(R.id.mapView)
-        button = findViewById<Button>(R.id.add_location)
-        mapView.onCreate(savedInstanceState)
-        mapView.setStyleUrl(getString(R.string.style_url))
+        mapView = findViewById<View>(R.id.rangeMapView) as MapView
+        //button = findViewById(R.id.add_location)
+        println("HERE")
+        mapView!!.onCreate(savedInstanceState)
+
         //TODO implement onMapClickListener - then done.
-        mapView.getMapAsync(OnMapReadyCallback { mapboxMap ->
+
+        mapView!!.getMapAsync(OnMapReadyCallback { mapboxMap ->
             mapboxMap.addMarker(MarkerOptions()
                     .position(LatLng(52.4631, -113.7286))
                     .title("Location")
                     .snippet("YOU ARE HERE"))
         })
 
-        button.setOnClickListener(object: View.OnClickListener {
+        /*button.setOnClickListener(object: View.OnClickListener {
             override fun onClick(view: View): Unit {
                 val intent = Intent(applicationContext, AddLocationToTaskActivity::class.java)
                 intent.putExtra("LatLng", point)
                 //TODO send intent back [with coords] .
             }
-        })
+        })*/
+
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        mapView!!.onStart()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        mapView!!.onResume()
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        mapView!!.onPause()
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        mapView!!.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView!!.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView!!.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        mapView!!.onSaveInstanceState(outState!!)
     }
 }
--- app/src/main/java/ca/ualberta/taskr/NearbyTasksActivity.kt
+++ app/src/main/java/ca/ualberta/taskr/NearbyTasksActivity.kt
@@ -1,17 +1,5 @@
 package ca.ualberta.taskr
 
-//import android.support.v7.app.AppCompatActivity
-//import android.os.Bundle
-//import android.view.View
-//import butterknife.BindView
-//import butterknife.ButterKnife
-//
-//import com.mapbox.mapboxsdk.Mapbox
-//import com.mapbox.mapboxsdk.constants.Style
-//import com.mapbox.mapboxsdk.maps.MapView
-//import com.mapbox.mapboxsdk.maps.MapboxMap
-//import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
-
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -23,69 +11,6 @@ import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.annotations.MarkerOptions
-
-
-
-//class NearbyTasksActivity : AppCompatActivity() {
-//
-////    @BindView(R.id.rangemapView)
-////    lateinit var mapView: MapView
-//private var mapView: MapView? = null
-//
-//
-//    public override fun onStart() {
-//        super.onStart()
-//        mapView!!.onStart()
-//    }
-//
-//    public override fun onResume() {
-//        super.onResume()
-//        mapView!!.onResume()
-//    }
-//
-//    public override fun onPause() {
-//        super.onPause()
-//        mapView!!.onPause()
-//    }
-//
-//    public override fun onStop() {
-//        super.onStop()
-//        mapView!!.onStop()
-//    }
-//
-//    override fun onLowMemory() {
-//        super.onLowMemory()
-//        mapView!!.onLowMemory()
-//    }
-//
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        mapView!!.onDestroy()
-//    }
-//
-//    override fun onSaveInstanceState(outState: Bundle?) {
-//        super.onSaveInstanceState(outState)
-//        mapView!!.onSaveInstanceState(outState!!)
-//    }
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-////        super.onCreate(savedInstanceState)
-////        Mapbox.getInstance(this, getString(R.string.access_token))
-////        setContentView(R.layout.activity_nearby_tasks)
-////        ButterKnife.bind(this)
-////
-////        mapView.onCreate(savedInstanceState)
-////        mapView.setStyleUrl(getString(R.string.style_url))
-//
-//        //TODO - track location of user && include the 5KM range.
-//        super.onCreate(savedInstanceState)
-//        Mapbox.getInstance(this, "pk.eyJ1IjoiYmFybmFidXN0aGViZW5pZ24iLCJhIjoiY2pldWI2MHN2NGhrZDJxbWU4dHdubmwxYSJ9.ZVq95tHTxTgyyppAfj3Jdw")
-//        setContentView(R.layout.activity_nearby_tasks)
-//        mapView = findViewById<View>(R.id.rangemapView) as MapView
-//        mapView!!.onCreate(savedInstanceState)
-//
-//    }
-//}
 
 class NearbyTasksActivity : AppCompatActivity() {
     private var mapView: MapView? = null
