b2d75b1fd913c60cc3056fa472f215ef8cbad2fd
eyesniper2 <eyesniper2@rayfall.net>
2018-03-19 12:35:06 -0600

    Added more docs

--- app/src/main/java/ca/ualberta/taskr/util/PhotoConversion.kt
+++ app/src/main/java/ca/ualberta/taskr/util/PhotoConversion.kt
@@ -6,12 +6,26 @@ import android.util.Base64
 import ca.ualberta.taskr.exceptions.ImageTooLargeException
 import java.io.ByteArrayOutputStream
 
-// Referenced: https://www.thecrazyprogrammer.com/2016/10/android-convert-image-base64-string-base64-string-image.html
+/**
+ * A static utils class to covert photos from bitmaps to strings and the other way around.
+ * Referenced: https://www.thecrazyprogrammer.com/2016/10/android-convert-image-base64-string-base64-string-image.html
+ *
+ * @author eyesniper2
+ */
 class PhotoConversion {
     companion object{
 
         private const val MaxImageByte:Int = 65536
 
+        /**
+         * Get a base64 encoded string from a bitmap image
+         *
+         * @param bitmap Image to be encoded
+         *
+         * @return The base64 encoded string
+         *
+         * @throws ImageTooLargeException
+         */
         fun getPhotoString(bitmap:Bitmap): String{
             val byteArray = ByteArrayOutputStream()
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArray)
@@ -23,6 +37,13 @@ class PhotoConversion {
             return Base64.encodeToString(imageBytes, Base64.DEFAULT)
         }
 
+        /**
+         * Get a bitmap photo from a base64 encoded string
+         *
+         * @param imageString base64 encoded image string
+         *
+         * @return Return a bitmap image
+         */
         fun getBitmapFromString(imageString:String): Bitmap{
             val imageBytes = Base64.decode(imageString, Base64.DEFAULT)
             return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
