10bcc7492e62edfedc36e085cb24a529d8c2a0cb
Michael Steer <steer@ualberta.ca>
2018-03-19 12:53:24 -0600

    fixed a broken arraylist

--- app/src/main/java/ca/ualberta/taskr/models/Task.kt
+++ app/src/main/java/ca/ualberta/taskr/models/Task.kt
@@ -7,7 +7,7 @@ import java.io.Serializable
  * Task Class
  */
 data class Task(val owner: String, val title: String, var status: TaskStatus?,
-                val bids: ArrayList<Bid>, val description: String, val photos: ArrayList<Image>,
+                val bids: ArrayList<Bid>, val description: String, val photos: ArrayList<String>,
                 val location: LatLng?, var chosenBidder: String ): Serializable {
 
     fun getBidAtIndex(index: Int): Bid {
