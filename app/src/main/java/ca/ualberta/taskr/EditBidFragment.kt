eabdcb6e83799faad1a732b9cd33076929a5ab12
eyesniper2 <eyesniper2@rayfall.net>
2018-03-21 16:24:21 -0600

    Fixed edit bid crashing

--- app/src/main/java/ca/ualberta/taskr/EditBidFragment.kt
+++ app/src/main/java/ca/ualberta/taskr/EditBidFragment.kt
@@ -32,17 +32,6 @@ class EditBidFragment : DialogFragment() {
     lateinit var fragmentTitle : TextView
     private var mListener: EditBidFragmentInteractionListener? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (arguments != null) {
-            if (arguments.getString("DISPLAYBID") != null) {
-                var strBid = arguments.getString("DISPLAYBID")
-                fragmentTitle.text = getString(R.string.fragment_edit_bid)
-                displayBid = GenerateRetrofit.generateGson().fromJson(strBid, Bid::class.java)
-            }
-        }
-    }
-
     /**
      * Creates the view for EditBidFragment's layout, then updates displayed bid amount with
      * value obtained from a provided Bid object. If no Bid was provided, no update is made.
@@ -61,6 +50,13 @@ class EditBidFragment : DialogFragment() {
             // If displayBid not initialized, then no Bid object was provided by
             // container activity.
             Log.e("DisplayBid Object", "No Bid object provided")
+        }
+        if (arguments != null) {
+            if (arguments.getString("DISPLAYBID") != null) {
+                var strBid = arguments.getString("DISPLAYBID")
+                fragmentTitle.text = getString(R.string.fragment_edit_bid)
+                displayBid = GenerateRetrofit.generateGson().fromJson(strBid, Bid::class.java)
+            }
         }
         return view
     }
