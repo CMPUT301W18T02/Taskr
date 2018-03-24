acaf46cb619e77397d4170c9687fea243cee1e97
James Cook <mrniceguyjames@gmail.com>
2018-03-24 15:58:09 -0600

    Added stubs for tests of Task Basics.

---
+++ app/src/androidTest/java/ca/ualberta/taskr/TaskBasicsTest.kt
@@ -0,0 +1,30 @@
+package ca.ualberta.taskr
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+/**
+ * Created by mrnic on 2018-03-24.
+ */
+@RunWith(RobolectricTestRunner::class)
+class TaskBasicsTest {
+
+    @Test
+    fun addATask(){}
+
+    @Test
+    fun maxLengthOfTaskTitle(){}
+
+    @Test
+    fun maxLengthOfTaskDesc(){}
+
+    @Test
+    fun viewListOfMyTasks(){}
+
+    @Test
+    fun editDescription(){}
+
+    @Test
+    fun delTask(){}
+}
