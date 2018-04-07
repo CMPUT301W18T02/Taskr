e67cbe68dd6a74e3e476513d837a07ea7101a034
MichaelSteer <swedester44@gmail.com>
2018-04-07 16:22:18 -0600

    Added Documentation for Exceptions

--- app/src/main/java/ca/ualberta/taskr/exceptions/UserAlreadyExistsException.kt
+++ app/src/main/java/ca/ualberta/taskr/exceptions/UserAlreadyExistsException.kt
@@ -1,4 +1,7 @@
 package ca.ualberta.taskr.exceptions
 
-
+/**
+ * UserAlreadyExistsException. This is thrown when a [User] already exists
+ * @exception UserAlreadyExistsException
+ */
 class UserAlreadyExistsException(message: String) : Exception("User: $message already exists")
--- app/src/main/java/ca/ualberta/taskr/exceptions/UserDoesNotExistException.kt
+++ app/src/main/java/ca/ualberta/taskr/exceptions/UserDoesNotExistException.kt
@@ -1,4 +1,7 @@
 package ca.ualberta.taskr.exceptions
 
-
+/**
+ * UserDoesNotExistException. This is thrown when a [User] does not exist
+ * @exception UserDoesNotExistException
+ */
 class UserDoesNotExistException(message: String) : Exception("User: $message does not exist")
