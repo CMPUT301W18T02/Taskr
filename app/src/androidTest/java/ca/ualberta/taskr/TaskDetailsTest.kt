ac22e07ce1722df1fcb1772fc20ec0785578e20c
Marissa Snihur <msnihur@ualberta.ca>
2018-04-09 15:12:26 -0600

    SearchTests Pass

--- app/src/androidTest/java/ca/ualberta/taskr/TaskDetailsTest.kt
+++ app/src/androidTest/java/ca/ualberta/taskr/TaskDetailsTest.kt
@@ -8,6 +8,7 @@ import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.action.ViewActions.typeText
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers
