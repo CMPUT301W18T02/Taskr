bfc9c8d8e025fbb22b83fb03aa3c0c0132c8bc54
Marissa Snihur <msnihur@ualberta.ca>
2018-04-08 18:59:24 -0600

    Added TaskAssignedTests

---
+++ app/src/androidTest/java/ca/ualberta/taskr/TaskAssignedTests.kt
@@ -0,0 +1,12 @@
+package ca.ualberta.taskr
+
+import org.junit.runner.RunWith
+
+/**
+ * Created by marissasnihur on 2018-04-08.
+ */
+
+@RunWith(RobolectricTestRunner::class)
+class TaskAssignedTests {
+    
+}
